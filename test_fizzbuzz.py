752a87db332fb7a78b49b5263dab3f505e183eed
Mason Lin <pizza0117@gmail.com>
2021-10-10 12:53:36 +0800

    test: :white_check_mark: add test, 6

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,7 +4,7 @@ import pytest
 
 # ,2,3,4,5
 @pytest.mark.parametrize(
-    "num, expected", [(1, "1"), (2, "2"), (3, "fizz"), (4, "4"), (5, "buzz")]
+    "num, expected", [(1, "1"), (2, "2"), (3, "fizz"), (4, "4"), (5, "buzz"), (6, "fizz")]
 )
 def test_fizzbuzz(num, expected):
     assert fizzbuzz.result(num) == expected
