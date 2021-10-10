84237dcf88cf5863933658625031caaa84942853
Mason Lin <pizza0117@gmail.com>
2021-10-10 12:45:20 +0800

    test: :test_tube: 5 should be buzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,3 +5,5 @@ def test_fizzbuzz():
     assert fizzbuzz.result(1) == "1"
     assert fizzbuzz.result(2) == "2"
     assert fizzbuzz.result(3) == "fizz"
+    assert fizzbuzz.result(4) == "4"
+    assert fizzbuzz.result(5) == "buzz"
